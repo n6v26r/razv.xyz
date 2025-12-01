548e8c6d9b04bfb7a34fe72d37d5aa06ca419a81
n6v26r <dedu.razvan.matei@gmail.com>
2025-12-01 03:40:28 +0200

    preload title image

--- src/lib/components/ThemedImage.svelte
+++ src/lib/components/ThemedImage.svelte
@@ -1,10 +1,31 @@
 <script>
   import { page } from "$app/state";
   import { theme } from "$lib/stores/theme";
+  import { onMount } from "svelte";
 
   export let name;
   export let path = page.url.pathname;
   export let ext = "";
+  export let preload = false;
+
+  let src = "";
+  let loaded = !preload;
+
+  $: src = `${path}/${name}_catppuccin-${$theme}${ext}`;
+
+  onMount(() => {
+    if (preload) {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        loaded = true;
+      };
+    }
+  });
 </script>
 
-<img src="{path}/{name}_catppuccin-{$theme}{ext}" alt={name} {...$$restProps}/>
+{#if loaded}
+  <img src="{src}" alt={name} {...$$restProps} />
+{:else}
+  <div style="width: 100%; height: 100%; background: var(--base);"></div>
+{/if}
